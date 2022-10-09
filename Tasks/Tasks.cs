4f5c6f5b80dfdb4cc66d5a817daa3dcff56ace17
Konstantsin <Drakula3D@mail.ru>
2022-10-09 20:17:04 +0300

    Создали новый класс Tasks где будут храниться решения наших задач

---
+++ Tasks/Tasks.cs
@@ -0,0 +1,13 @@
+﻿internal class Tasks
+{
+    public Tasks()
+    {
+    }
+
+    public void task1(int num1, int num2)
+    {
+        if (num1 == num2) Console.WriteLine($"Число а = {num1} равно числу b = {num2}");
+        else if (num1 > num2) Console.WriteLine($"Число а = {num1} > числа b = {num2}");
+        else Console.WriteLine($"Число а = {num1} < числа b = {num2}");
+    }
+}
